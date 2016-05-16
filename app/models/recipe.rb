7c2b1ece97b9c7130a4978250a677e00232633cc
UcielAlcala <uci_el@hotmail.com>
2016-05-16 16:52:10 -0500

    Initial test

---
+++ app/models/recipe.rb
@@ -0,0 +1,15 @@
+ENV['FOOD2FORK_KEY']="3b60963ab106f785f67b258fe5a9daa2"
+
+class Recipe
+ include HTTParty
+  key_value = ENV['FOOD2FORK_KEY'] 
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  base_uri "http://#{hostport}/api"
+  default_params key: key_value
+  
+  format :json
+
+	def self.for term
+		get('/search',query: {q:term})["recipes"]
+	end
+end
